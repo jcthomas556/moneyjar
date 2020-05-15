262f33693b349afb77adb7286d3276f3347806bb
Jacob Thomas <jcthomas556@gmail.com>
2020-05-15 16:01:18 -0600

    database connector file

--- web/Landing.php
+++ web/Landing.php
@@ -1,25 +1,5 @@
 <?php
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+include('database_connection.php');
 ?>
 
 <!DOCTYPE html>
@@ -111,7 +91,10 @@ catch (PDOException $ex)
             
 
     <?php
-        $statement = $db->query('SELECT * FROM accounts');
+
+
+
+$statement = $db->query('SELECT * FROM accounts');
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         {
             echo $row['email'] . ' password: ' . $row['password'] . '<br/>';
