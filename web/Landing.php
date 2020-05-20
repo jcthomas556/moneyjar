2506abed45f3faadd2bf7be8568f89cbdbc504b8
Jacob Thomas <jcthomas556@gmail.com>
2020-05-20 16:19:01 -0600

    db queries

--- web/Landing.php
+++ web/Landing.php
@@ -22,7 +22,7 @@ include('database_connection.php');
 
     <?php 
          $email = $_POST["email"];
-        // $password = $_POST["password"];
+         $password = $_POST["password"];
         // $logInRequest = $db->prepare('SELECT user_id FROM accounts WHERE email=:email AND password =:password');
         // $logInRequest->bindValue(':email', $email, PDO::PARAM_INT);
         // $logInRequest->bindValue(':password', $password, PDO::PARAM_INT);
@@ -37,14 +37,12 @@ include('database_connection.php');
         // }
        
         foreach($db->query(
-            "SELECT user_id FROM accounts WHERE email=crypt('$email', email)", PDO::FETCH_ASSOC) as $holder)
+            "SELECT user_id FROM accounts WHERE email=crypt('$email', email) AND password=crypt('$password', password)", PDO::FETCH_ASSOC) as $holder)
             {
                 if($holder['user_id'] > 0){
                     echo 'Successfully logged in, welcome user ' . $holder['user_id'];
                 }
-            // echo "testTwo::";
-            // echo $holder['user_id'];
-            // echo "::hereTwo";
+            
             }
             
         //$logInRequest2 = $db->query("SELECT user_id FROM accounts WHERE email = crypt('$email', email)");
