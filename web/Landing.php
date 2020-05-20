e5ad176dbbe0c7472162cb840727472c5c7759d3
Jacob Thomas <jcthomas556@gmail.com>
2020-05-20 16:51:34 -0600

    db queries

--- web/Landing.php
+++ web/Landing.php
@@ -37,7 +37,7 @@ include('database_connection.php');
         // }
        
         foreach($db->query(
-            "SELECT user_id FROM accounts WHERE email=crypt('$email', email) AND passwords='$passwords'", PDO::FETCH_ASSOC) as $holder)
+            "SELECT user_id FROM accounts WHERE email=crypt('$email', email) AND passwords=crypt('$passwords', passwords)", PDO::FETCH_ASSOC) as $holder)
             {
                 if($holder['user_id'] > 0){
                     echo 'Successfully logged in, welcome user ' . $holder['user_id'];
