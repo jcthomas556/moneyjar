9ff469ef6be722a996a6b248a46e8866e6308295
Jacob Thomas <jcthomas556@gmail.com>
2020-05-20 16:23:03 -0600

    db queries

--- web/Landing.php
+++ web/Landing.php
@@ -22,7 +22,7 @@ include('database_connection.php');
 
     <?php 
          $email = $_POST["email"];
-         $password = $_POST["password"];
+        // $password = $_POST["password"];
         // $logInRequest = $db->prepare('SELECT user_id FROM accounts WHERE email=:email AND password =:password');
         // $logInRequest->bindValue(':email', $email, PDO::PARAM_INT);
         // $logInRequest->bindValue(':password', $password, PDO::PARAM_INT);
@@ -36,14 +36,28 @@ include('database_connection.php');
         //     echo "Login Failed";
         // }
        
-        foreach($db->query(
-            "SELECT user_id FROM accounts WHERE email=crypt('$email', email) AND password=crypt('$password', password)", PDO::FETCH_ASSOC) as $holder)
-            {
-                if($holder['user_id'] > 0){
-                    echo 'Successfully logged in, welcome user ' . $holder['user_id'];
+        // foreach($db->query(
+        //     "SELECT user_id FROM accounts WHERE email=crypt('$email', email)", PDO::FETCH_ASSOC) as $holder)
+        //     {
+        //         if($holder['user_id'] > 0){
+        //             echo 'Successfully logged in, welcome user ' . $holder['user_id'];
+        //         }
+        //     // echo "testTwo::";
+        //     // echo $holder['user_id'];
+        //     // echo "::hereTwo";
+        //     }
+
+$pass = "password";
+            foreach($db->query(
+                "SELECT user_id FROM accounts WHERE email=crypt('$email', email) AND password=crypt('$password', password)", PDO::FETCH_ASSOC) as $holder)
+                {
+                    if($holder['user_id'] > 0){
+                        echo 'Successfully logged in, welcome user ' . $holder['user_id'];
+                    }
+                // echo "testTwo::";
+                // echo $holder['user_id'];
+                // echo "::hereTwo";
                 }
-            
-            }
             
         //$logInRequest2 = $db->query("SELECT user_id FROM accounts WHERE email = crypt('$email', email)");
         // echo "Look here" . $logInRequest;
