c5189657166c37532eab62763b98bbeeb32a718d
Jacob Thomas <jcthomas556@gmail.com>
2020-06-10 16:25:40 -0600

    Accounts

--- web/Jars.php
+++ web/Jars.php
@@ -47,40 +47,35 @@ $userID = $_SESSION['user_id'];
 
         <tr>
             <td>Jars</td>
+
             <td>
-            <select name="jars">
-            <?php 
-            echo "made it to 1";
+                <select name="jars">
+                    <?php 
+                    echo "made it to 1";
 
-            // $sql = mysqli_query($connection, "SELECT username FROM users");
-            foreach($db->query(
-                "SELECT jar_total, jar_active, user_id, jar_name FROM jars WHERE user_id = '1'", PDO::FETCH_ASSOC) as $holder)
-                {
-                    if($holder['jar_active'] == 't'){
+                    // $sql = mysqli_query($connection, "SELECT username FROM users");
+                    foreach($db->query(
+                        "SELECT jar_total, jar_active, user_id, jar_name FROM jars WHERE user_id = '1'", PDO::FETCH_ASSOC) as $holder)
+                        {
+                            if($holder['jar_active'] == 't'){
+                                
+                            }
+                    ?>
+                    <option value="jar1"><?php echo $holder['jar_name']; ?></option>
                         
-                    }
-            ?>
-            <option value="jar1"><?php echo $holder['jar_name']; ?></option>
-                    
-            <?php } ?>
-            <!-- This allows the php to end after the subject is inserted, so this subject of the jar names is inside the php loop -->
-           
+                    <?php } ?> </select>
+                    <!-- This allows the php to end after the subject is inserted, so this subject of the jar names is inside the php loop -->
             </td>
-            </tr>
+        </tr>
 
-            <img src = "https://images.collectivesupply.com/wp-content/uploads/2017/11/12140507/10-oz-glass-jar.png" alt="jar">
-
-        
-       
+            
+ 
     </div>
 
     <div class="container">
-        <div class="text-center">
-            
-
-
-                
-                                
+        <div class="text-center">            
+            <img src="https://images.collectivesupply.com/wp-content/uploads/2017/11/12140507/10-oz-glass-jar.png" alt="Trulli" width="500" height="333">
+              
                             </div>
                         </div>
                     </form>
