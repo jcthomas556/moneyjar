ba8e94396bcb178e80a15354b452c14220e9515c
Jacob Thomas <jcthomas556@gmail.com>
2020-06-10 16:59:31 -0600

    Accounts

--- web/Jars.php
+++ web/Jars.php
@@ -63,7 +63,7 @@ $userID = $_SESSION['user_id'];
                                     
                                 }
                         ?>
-                        <option value=<?php $holder['jar_name']; ?>><?php echo $holder['jar_name']; ?></option>
+                        <option value="test"><?php echo $holder['jar_name']; ?></option>
                             
                         <?php } ?> </select>
                         <!-- This allows the php to end after the subject is inserted, so this subject of the jar names is inside the php loop -->
