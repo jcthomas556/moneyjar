4fdb42b91c8a51b8fa5a79bbebdcd2bcb7027bd6
Jacob Thomas <jcthomas556@gmail.com>
2020-06-11 16:46:25 -0600

    Accounts

--- web/Landing.php
+++ web/Landing.php
@@ -56,7 +56,7 @@ include('database_connection.php');
                         $_SESSION['username'] = $userName;
                         $_SESSION['user_id'] = $userID;
 
-                        $successMessage = "Successfully logged in, welcome, " . $holder['user_name'];
+                        $successMessage = "<p id='successMessage'> Successfully logged in, welcome, " . $holder['user_name'] "</p";
                                             
                         // echo "<p id='signedInMessage';>";
                         // echo 'Successfully logged in, welcome, ' . $holder['user_name'];
