06e27cc4644a32d63c16597ddec30d8572901c70
Jacob Thomas <jcthomas556@gmail.com>
2020-06-17 15:58:29 -0600

    Accounts

--- web/Jars.php
+++ web/Jars.php
@@ -101,12 +101,17 @@ $userID = $_SESSION['user_id'];
                             true,
                             '$newJarName')"
                         );
-                    }
-                    // INSERT INTO users_jars(user_id, jar_id)
-                    // VALUES(
-                    //     '$userID,
-                    //     (SELECT jar_id FROM jars WHERE jar_owner_id = '$userID' AND jar_name = '$newJarName)
-                    // )");
+                    
+                $db->query(
+                    "INSERT INTO users_jars(user_id, jar_id)
+                    VALUES(
+                        '$userID,
+                        (SELECT jar_id FROM jars WHERE jar_owner_id = '$userID' AND jar_name = '$newJarName)
+                    )"
+                );
+            }
+                
+                    
                 
                //insert statement for jar
                //might need to pass the session variables into this function for proper user
