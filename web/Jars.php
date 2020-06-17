ad106b7c72eed9c1f69085b5b52f518297992d06
Jacob Thomas <jcthomas556@gmail.com>
2020-06-17 17:09:22 -0600

    Accounts

--- web/Jars.php
+++ web/Jars.php
@@ -100,13 +100,13 @@ $userID = $_SESSION['user_id'];
                         );
 
                     //linkJar($newJarName, $userID, $db);
-                    $select = "(SELECT jar_id FROM jars WHERE jar_owner_id = '$userID' AND jar_name = '$newJarName')";
+                    //$select = "(SELECT jar_id FROM jars WHERE jar_owner_id = '$userID' AND jar_name = '$newJarName')";
             
                     $db->query(
                         "INSERT INTO users_jars (user_id, jar_id)
                         VALUES(
                             '$userID',
-                            $select
+                            SELECT jar_id FROM jars WHERE jar_owner_id = '$userID' AND jar_name = '$newJarName'
                         )"
                     );  
                              
