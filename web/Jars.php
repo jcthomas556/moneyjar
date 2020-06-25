0f8ee85e8ef7b3fd46dab3a2f70b32918f403072
Jacob Thomas <jcthomas556@gmail.com>
2020-06-25 16:30:51 -0600

    Accounts

--- web/Jars.php
+++ web/Jars.php
@@ -100,14 +100,21 @@ $userID = $_SESSION['user_id'];
                 
                 if(isset($_GET['addMoney'])){
                     echo "jar total =";
-                    echo $jar_total;
+                    setlocale(LC_MONETARY, 'en_US');
+                    echo money_format('%(#10n', $jar_total) . "\n";
+                    $addNum = money_format('%(#10n', $jar_total);
 
-                    echo "new jar total =";
-                     $newjar_total = $jar_total + '1.00';
-                     echo $newjar_total;
-                     $testNum = $jar_total;
-                     $testNum = $testNum + 1;
-                     echo $testNum;
+
+                   
+                    // echo $jar_total;
+
+                     echo "new jar total =";
+                     echo $addNum + 1;
+                    //  $newjar_total = $jar_total + '1.00';
+                    //  echo $newjar_total;
+                    //  $testNum = $jar_total;
+                    //  $testNum = $testNum + 1;
+                    //  echo $testNum;
                     $db->query(
                         "UPDATE jars
                         SET 'jarTotal' = '$testNum' + '$1'
