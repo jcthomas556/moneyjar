04681b7a4c1884012aa4dd43576d7bce87b8b4fb
Jacob Thomas <jcthomas556@gmail.com>
2020-06-26 16:20:25 -0600

    Accounts

--- web/Jars.php
+++ web/Jars.php
@@ -64,7 +64,7 @@ $userID = $_SESSION['user_id'];
                             }
                             
                         ?>
-                        <option value="<?php echo $holder['jar_total']; ?>"><?php echo $holder['jar_name']; $currJarID=$holder['jar_id']; $inviteCode=$holder['jar_invite_code'];?></option>
+                        <option value="<?php echo $holder['jar_total']; ?>"><?php echo $holder['jar_name']; $currJarID=$holder['jar_id']; echo $holder['jar_invite_code'];?></option>
                         
                             
                         <?php }  $jar_total = $holder['jar_total']; ?> </select>
