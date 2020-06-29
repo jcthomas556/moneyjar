0a597275db9f4da1a15bae020bc66b7496e446ec
Jacob Thomas <jcthomas556@gmail.com>
2020-06-29 17:54:17 -0600

    Accounts

--- web/Jars.php
+++ web/Jars.php
@@ -104,16 +104,18 @@ $userID = $_SESSION['user_id'];
                     }
 
                 if(array_key_exists('addMoney', $_GET)) { 
-                    $valid = true;
-                    echo "array key stop = ";
-                    echo $inviteCode;
+                    echo "success 1";
+                    // $valid = true;
+                    // echo "array key stop = ";
+                    // echo $inviteCode;
                     // echo $inviteCode;
                     // echo $jar_total;
                     // addMoney($inviteCode, $db, $jar_total);
                 }    
                 if ($valid == TRUE){
-                    echo "valid stop = ";
-                    echo $inviteCode;
+                    echo "success 2";
+                    // echo "valid stop = ";
+                    // echo $inviteCode;
                 }
 
                 echo "<p> $";
