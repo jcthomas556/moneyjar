6b454abd15f936fc661efcddb9827e3b306d2e00
Jacob Thomas <jcthomas556@gmail.com>
2020-07-01 17:35:06 -0600

    Accounts

--- web/AccountPage.php
+++ web/AccountPage.php
@@ -62,19 +62,19 @@ $userID = $_SESSION['user_id'];
         // echo 'this is my email' . $email;
         // echo $email;
 
-        foreach($db->query(
-                    "SELECT user_id, user_name FROM accounts WHERE email=crypt('$email', email) AND passwords=crypt('$passwords', passwords)", PDO::FETCH_ASSOC) as $holder)
-                    {
-                        if($holder['user_id'] > 0){
-                            echo "test";
-                            echo 'Account created! Successfully logged in, welcome, ' . $holder['user_name'];
+            foreach($db->query(
+                        "SELECT user_id, user_name FROM accounts WHERE email=crypt('$email', email) AND passwords=crypt('$passwords', passwords)", PDO::FETCH_ASSOC) as $holder)
+                        {
+                            if($holder['user_id'] > 0){
+                                echo "test";
+                                echo 'Account created! Successfully logged in, welcome, ' . $holder['user_name'];
+                            }
                         }
-                    }
-
-
-        }
 
         header('Location: Jars.php');
+        }
+
+        
     ?>
     
     
