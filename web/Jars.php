ac6346cb530c2950bc0382caff4e410cf3f97e54
Jacob Thomas <jcthomas556@gmail.com>
2020-06-30 18:41:52 -0600

    Accounts

--- web/Jars.php
+++ web/Jars.php
@@ -277,8 +277,8 @@ $userID = $_SESSION['user_id'];
             <img src="https://images.collectivesupply.com/wp-content/uploads/2017/11/12140507/10-oz-glass-jar.png" alt="Trulli" width="500" height="433">
 
             <br>
-            <form  method="GET">
-                <button type="submit" name="addMoney" value="non ajax button - Put $1 in the Jar" class="btn btn-success"> non ajax button</button>
+            <form method="GET">
+                <button type="submit" name="addMoney"  class="btn btn-success"> non ajax button</button>
             </form>
 
 
